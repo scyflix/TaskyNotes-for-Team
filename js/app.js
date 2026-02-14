30ec3aa2fd32094547cfe2eed1a06ef94a4585cd
abdulroqibscybud <abdulroqeeboladipo@gmail.com>
2026-02-14 17:46:40 +0100

    try: fix component file path bug

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ import { attachSidebarToggle } from "./utils.js";
 import { openCreateTaskModal, openLogTaskModal, openCreateWorkspaceModal } from "./task.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
-  await loadComponent("/components/sidebar-admin.html", "adminSidebarContainer");
+  await loadComponent("../components/sidebar-admin.html", "adminSidebarContainer");
   await loadComponent(
-    "/components/sidebar-member.html",
+    "../components/sidebar-member.html",
     "memberSidebarContainer",
   );
    await loadComponent(
-    "/components/sidebar-dashboard.html",
+    "../components/sidebar-dashboard.html",
     "dashboardSidebarContainer",
   );
 
